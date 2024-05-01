c1029804e880b0b68050fb3fb1769749fff2b98f
hitonichi <42115291+hitonichi@users.noreply.github.com>
2024-05-02 03:27:53 +0700

    feat: add TDD testcase

--- tests/calendar/test_bv.py
+++ tests/calendar/test_bv.py
@@ -1,4 +1,5 @@
 
+import csv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -6,8 +7,10 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 import time
+import pytest
 
 from utils.auth import login
+from utils.readData import readDataFromExcel
 
 def setSelect(driver, eleId, value):
     selEle = driver.find_element(By.ID, eleId)
@@ -226,5 +229,64 @@ def test_bv5():
 
     modalEle = driver.find_element(By.CLASS_NAME, "modal")
     assert not modalEle.is_displayed()
+
+    driver.quit()
+
+def parseDate(rawDate):
+    return rawDate.split(',')
+
+test_data = readDataFromExcel("calendar_bva")
+@pytest.mark.parametrize("startDate,endDate,isValid", test_data)
+def test_bv6(startDate, endDate, isValid):
+    start_time = parseDate(startDate)
+    end_time = parseDate(endDate)
+    
+    print('[DBG]', start_time, end_time, isValid)
+    
+    driver = webdriver.Chrome()
+
+    wait = WebDriverWait(driver, 10)
+
+    login(driver, "teacher", "moodle")
+
+    driver.get("https://school.moodledemo.net/my/index.php")
+
+    driver.implicitly_wait(2)
+    newEventBtn = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[data-action='new-event-button']")))
+
+    newEventBtn.click()
+
+    saveEventBtn = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[data-action='save']")))
+    setValueById(driver, "id_name", "Test Event")
+    driver.find_element(By.CLASS_NAME, "moreless-toggler").click()
+    driver.implicitly_wait(2)
+    driver.find_element(By.ID, "id_duration_1").click()
+
+    setSelect(driver, "id_timestart_day", start_time[0])
+    setSelect(driver, "id_timestart_month", start_time[1])
+    setSelect(driver, "id_timestart_year", start_time[2])
+    setSelect(driver, "id_timestart_hour", start_time[3])
+    setSelect(driver, "id_timestart_minute", start_time[4])
+
+    setSelect(driver, "id_timedurationuntil_day", end_time[0])
+    setSelect(driver, "id_timedurationuntil_month", end_time[1])
+    setSelect(driver, "id_timedurationuntil_year", end_time[2])
+    setSelect(driver, "id_timedurationuntil_hour", end_time[3])
+    setSelect(driver, "id_timedurationuntil_minute", end_time[4])
+
+
+    if (isValid == 'N'):
+        saveEventBtn.click()
+        wait.until(EC.invisibility_of_element_located((By.CSS_SELECTOR, "button[data-action='save'] > span[data-region='loading-icon-container']")))
+        time.sleep(5) # Has to wait for a while due to the lack of UI synchronization of the website
+
+        durationErrorMsg = driver.find_element(By.ID, "fgroup_id_error_durationgroup")
+        assert durationErrorMsg.is_displayed() 
+    else:
+        saveEventBtn.click()
+        time.sleep(5)
+
+        modalEle = driver.find_element(By.CLASS_NAME, "modal")
+        assert not modalEle.is_displayed()
 
     driver.quit()
